593bf1eacf47747362cc3cd1f300cccec8789121
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2024-09-25 16:02:39 -0700

    installed fontawesome

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['uuid'],  // Explicitly externalize 'uuid'
+      external: [
+        'uuid',  // Externalize 'uuid'
+        '@fortawesome/react-fontawesome'  // Explicitly externalize FontAwesome package
+      ]
     }
   }
-})
+});
